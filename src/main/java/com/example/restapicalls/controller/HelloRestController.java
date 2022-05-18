893eb6b9c0d67a5a51d03d388245cc962e70da12
Your Name <you@example.com>
2022-05-19 01:05:56 +0530

    Added: Make REST Call to show Hello Mark

--- src/main/java/com/example/restapicalls/controller/HelloRestController.java
+++ src/main/java/com/example/restapicalls/controller/HelloRestController.java
@@ -1,21 +1,33 @@
 package com.example.restapicalls.controller;
-
+/*
+ *  import classes
+ */
+import com.example.restapicalls.model.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/web")
-
+/*
+ * create a class name as HelloRestController
+ */
 public class HelloRestController {
-    @GetMapping("/message")
-    public String message() {
+        @GetMapping("/message")
+        public String message() {
         return "Hello From BridgeLabz";
     }
         @GetMapping("/query")
         public String sayHello(@RequestParam String name) {
             return "Hello "+name+" From BridgeLabz";
     }
-    @GetMapping("/param/{name}")
-    public String sayHelloParam(@PathVariable String name) {
+        @GetMapping("/param/{name}")
+        public String sayHelloParam(@PathVariable String name) {
         return "Hello "+name+" From BridgeLabz";
     }
+        @PostMapping("/post")
+        public String userData(@RequestBody User user) {
+        return "Hello " + user.getfirstName() + " " + user.getlastName() + " from BridgeLabz";
+    }
 }
+
+
+
---
+++ src/main/java/com/example/restapicalls/model/User.java
@@ -0,0 +1,20 @@
+package com.example.restapicalls.model;
+
+
+public class User {
+    private String firstName;
+    private String lastName;
+    public String getfirstName() {
+        return firstName;
+    }
+    public void setfirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getlastName() {
+        return lastName;
+    }
+    public void setlastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
+
